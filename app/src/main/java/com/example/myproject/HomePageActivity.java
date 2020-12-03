c737f10995d122b4f7bc88f85f76349f79e93d73
helloiris0216 <l66074106@gs.ncku.edu.tw>
2020-12-03 22:17:10 +0800

    Feat:bottomNavigation setting, activity switch 1.顯示導覽列 2.更換導覽列的顏色 3.activities:homePage, diary, user info 4.activity跳轉成功 & setTitle
    Issue #001

---
+++ app/src/main/java/com/example/myproject/HomePageActivity.java
@@ -0,0 +1,18 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        setTitle("HOME");
+
+
+    }
+}
