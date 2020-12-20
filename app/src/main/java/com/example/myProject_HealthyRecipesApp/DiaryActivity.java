9d700dbb09774bb1acb330dadaf08386f2f9a7ef
helloiris0216 <l66074106@gs.ncku.edu.tw>
2020-12-20 23:22:15 +0800

    Feat:[修復]將資料內容顯示在listView上指定的item列；[BUG]照片上傳;[新增、BUG]計算機功能
    Issue #015

--- app/src/main/java/com/example/myProject_HealthyRecipesApp/DiaryActivity.java
+++ app/src/main/java/com/example/myProject_HealthyRecipesApp/DiaryActivity.java
@@ -1,8 +1,8 @@
 package com.example.myProject_HealthyRecipesApp;
-//TODO:[目標] 本頁面的功能是紀錄使用者每天的飲食，並計算出食物中的營養素和熱量。
-//TODO:[目標] 建立 action bar。
-//TODO:[目標] 按下 "加入食物" 後，會跳至另一個顯示食物的頁面，讓使用者點選食物。
-//TODO:[目標] (從firebase上拉資料)會將食物名稱和所對應的熱量顯示在 textView 上。
+//TODO:[目標] 本頁面的功能是紀錄使用者每天的飲食，並計算出食物中的熱量。
+//TODO:[目標:完成] 建立 action bar。
+//TODO:[目標:完成] 按下 "加入食物" 後，會跳至另一個顯示食物的頁面，讓使用者點選食物。
+//TODO:[目標:完成] (從firebase上拉資料)會將食物名稱和所對應的熱量顯示在 textView 上。
 //TODO:[目標] 顯示每餐食物的總熱量。
 //TODO:[目標] 將一天的總熱量做加總。
 //TODO:[目標] 日記需要有修改和刪除的功能，修改->menu；刪除->長按 listView。
@@ -22,6 +22,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -42,7 +43,7 @@ public class DiaryActivity extends AppCompatActivity {
     private static String[] arr_mealName, arr_foodName;
     private Context context;
     private ListView listView_diary;
-    private TextView tv_meal_d, tv_addFood_d, tv_cal_d, tv_food_d;
+    private TextView tv_meal_d, tv_addFood_d, tv_cal_d, tv_food_d, tv_remaining, tv_foodData;
     private HashMap<String, Object> meal_data;
     private String TAG = "diary_activity";
     private List<Map<String, Object>> list_meal;
@@ -59,6 +60,9 @@ public class DiaryActivity extends AppCompatActivity {
     String pt;
     String carbs;
     String fat;
+    private Double total, cal_c;
+    private EditText et_goal;
+
 
     //TODO:初始化
     @Override
@@ -151,7 +155,18 @@ public class DiaryActivity extends AppCompatActivity {
                 meal_data.put("MEALNAME", arr_mealName[i]);
                 meal_data.put("FOOD", arr_foodName[i]);
                 list_meal.add(meal_data);
+
+                //1.取得每餐中每樣食物的 cal
+                cal_c = Double.parseDouble(cal);
+                Log.d(TAG, "cal:"+cal_c);
+
+                //2.將每樣食物的 cal 加總
+                total= 0.0;
+                total += cal_c;
+                Log.d(TAG, "total:"+total);
+
             }
+
 
             //[1]-2.將要顯示的清單存入到 adapter
             final SimpleAdapter adapterNew = new SimpleAdapter(context, list_meal, R.layout.diary_listview_item_layout,
@@ -159,7 +174,46 @@ public class DiaryActivity extends AppCompatActivity {
                     new int[]{R.id.tv_meal_d, R.id.tv_food_d});
             //連結adapter
             listView_diary.setAdapter(adapterNew);
+
+            //TODO:[未完成]計算機功能
+            //3.將每樣食物的 cal 加總顯示在每餐的 tv_cal_d
+            tv_cal_d = findViewById(R.id.tv_cal_d);
+            try {
+                tv_cal_d.setText("");
+                Log.d(TAG, "total:"+total.toString());
+                tv_cal_d.setText(total.toString());
+
+                //4.加總整天的 tv_cal_d，並將結果顯示在 tv_foodData
+                Double totalCal_allDay = Double.parseDouble(tv_cal_d.getText().toString());
+                Double total_allDay = 0.0;
+                for (int i=0; i<=3; i++){
+                    totalCal_allDay += total_allDay;
+                    Log.d(TAG, "total_allDay:"+total_allDay);
+                }
+                tv_foodData = findViewById(R.id.tv_foodData);
+                tv_foodData.setText(totalCal_allDay.toString());
+
+                //5.取得使用者輸入的目標熱量 et_goal -> 未輸入:toast
+                et_goal = findViewById(R.id.et_goal);
+                Double goal = Double.parseDouble(et_goal.getText().toString());
+
+                //6.計算: remaining = goal (et_goal) - cal (tv_foodData)
+                Double cal = Double.parseDouble(tv_foodData.toString());
+                Double remaining = 0.0;
+                remaining = goal - cal;
+
+                //7.將計算結果顯示在 tv_remaining(結束)
+                tv_remaining = findViewById(R.id.tv_remaining);
+                tv_remaining.setText(remaining.toString());
+
+            } catch (Exception e) {
+                Toast.makeText(context, "請輸入每日目標熱量", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+
         }
+
+
 
         //[1]-3 & [2]-5.監聽是listView上哪列item被點選
         listView_diary.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -168,11 +222,14 @@ public class DiaryActivity extends AppCompatActivity {
                 //[★★★] 紀錄被點選的項目編號
                 clickedItem = position;
 
+
                 //跳頁
                 Intent intent = new Intent(context, FoodDataActivity.class);
                 startActivity(intent);
             }
         }); //end listener
-    }   //end findAndPutData()
+    }//end findAndPutData()
 
-}   //end activity
+
+
+}//end activity
